f15933a6ffad714a052c5de19f2a8b0c967f5eca
mouxyGM <167781901+mouxyGM@users.noreply.github.com>
2024-08-28 11:59:20 -0400

    Update Bradford_standard_curve.py
    added comments to the code for ease of following

--- Bradford_standard_curve.py
+++ Bradford_standard_curve.py
@@ -16,13 +16,17 @@ limitations under the License.
 
 
 
-# This script intends to run a Bradford assay standard curve on Opentrons OT2.
-# Note that this script only runs a reference standard as of now. Additional edits required for sample manipulation.
-# Standard stock in B1 and B2 of tube_rack. SSS in A1 of tube_rack.
-# This run takes 1 hr
+'''
+This script intends to run a Bradford assay standard curve on Opentrons OT2.
+Note that this script only runs a reference standard as of now.
 
-#changing the script to stamp dye onto the entire final plate then the stamping plate onto the entire final plate
+Volumes of reagents needed:
+1.5mL of Reference Standard Stock on tube rack A1;
+1mL of SSS on tube rack A2
+13mL of Bradford assay reagent
+'''
 
+#importing the API
 from opentrons import protocol_api
 import numpy as np
 
@@ -30,21 +34,21 @@ metadata = {'apiLevel': '2.12'}
 
 def run(protocol: protocol_api.ProtocolContext):
 
-    tube_rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',1)
-    final_plate = protocol.load_labware('corning_96_wellplate_360ul_flat', 3)
-    diluent = protocol.load_labware('axygen_1_reservoir_90ml', 4)
-    pierce_reagent = protocol.load_labware('axygen_1_reservoir_90ml', 5)
-    stamping_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 6)
-
-    tiprack_1_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 7)
+    tube_rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',1) #Tube rack that holds the reference standard and the SSS goes on deck position #1
+    final_plate = protocol.load_labware('corning_96_wellplate_360ul_flat', 3) #Final plate (the plate to be read) goes on deck position #3
+    diluent = protocol.load_labware('axygen_1_reservoir_90ml', 4)  #Diluent reservoir goes on deck position #4
+    pierce_reagent = protocol.load_labware('axygen_1_reservoir_90ml', 5) #Bradford reagent reservoir goes on deck position #5
+    stamping_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 6) #Stamping plate (as defined in the manuscript) goes on deck position #6
+ 
+    tiprack_1_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 7) #four 300uL tip racks go on deck positions #7, 8, 9 and 10
     tiprack_2_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 8)  
     tiprack_3_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 9)
     tiprack_4_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 10)
 
-    p300 = protocol.load_instrument('p300_single_gen2', 'right',tip_racks=[tiprack_1_300, tiprack_2_300])
+    p300 = protocol.load_instrument('p300_single_gen2', 'right',tip_racks=[tiprack_1_300, tiprack_2_300])  #Using only the 300uL pipette (single pipette on the right, multichannel pipette on the left)
     p300_multi = protocol.load_instrument('p300_multi_gen2', 'left',tip_racks=[tiprack_3_300, tiprack_4_300])
 
-#add 500ul diluent to all wells in column 1-4
+#Step 1: Adding 500ul diluent to all wells in column 1-4 to the stamping plate
     p300_multi.pick_up_tip()
     for i in range(4):
         p300_multi.transfer(volume=250, source = diluent['A1'], dest = stamping_plate.columns()[i], new_tip='never',trash = False)
@@ -52,48 +56,52 @@ def run(protocol: protocol_api.ProtocolContext):
         p300_multi.transfer(volume=250, source = diluent['A1'], dest = stamping_plate.columns()[i], new_tip='never',trash = False)
     p300_multi.return_tip()
 
-# Step 1: adding 3 replicates of diluent and standard control to 75 (E1-3) 100 (F1-3) 150 (G1-3) and 200 (H1-3) mcg/ml points to the stamping plate 
+# Step 2: Preparing 3 replicates of the 75, 100, 150 and 200ug/mL standard points in the stamping plate
+    #75ug/mL: (E1-3), 100ug/mL: (F1-3); 150ug/mL (G1-3); 200ug/mL (H1-3) mcg/ml points to the stamping plate 
 
-#   dil_vol1 = [740,720,680,960] #diluent volume
+#   Total volume of diluent needed is 740,720,680, and 960 respectuively. However, due to the max capacity of the tips being 300uL, we break up the addition of the diluent in 3 different stages with volumes <300uL as in the array dil_vol1_b
     dil_vol1_b = [240,220,180,250] #we are doing this due to the maximum capacity of the tips being 300uL
-#   dil_vol1_c = 210
+#   remaining volume for wells H1, H2 and H3 is 210uL to reach 960uL total diluent
 
-    dest_1 = ['E1','F1','G1','H1']
+    dest_1 = ['E1','F1','G1','H1'] #well locations for 75-200 ug/mL standard points are in the arrats dest_1, dest_2 and dest_3
     dest_2 = ['E2','F2','G2','H2']
     dest_3 = ['E3','F3','G3','H3']
-    dest1 =[dest_1,dest_2,dest_3]
+    dest1 =[dest_1,dest_2,dest_3] #triplicating the destination since we are adding the standard in triplicate
 
-    samp_vol1=[60,80,120,240] #standard volume (these ratios make up the dilutions rangning from 75 to 200 mcg/mL)
+    samp_vol1=[60,80,120,240] #standard volume needed (these ratios make up the dilutions rangning from 75 to 200 mcg/mL)
 
     #replicating in 3
     for j in range (3):
         p300.pick_up_tip()      
         #dispensing diluent to stamping plate
         for i in range(len(dil_vol1_b)):
-            p300.transfer(volume = dil_vol1_b[i], source = diluent['A1'], dest = stamping_plate[dest1[j][i]], new_tip = 'never',trash = False)
-
+            p300.transfer(volume = dil_vol1_b[i], source = diluent['A1'], dest = stamping_plate[dest1[j][i]], new_tip = 'never',trash = False) 
+        #dispensing that remaining 210uL of diluent described in line 64 to the wells H1, H2 and H3 to reach a total of 960uL
         p300.transfer(volume = 210, source = diluent['A1'], dest = stamping_plate[dest1[j][3]], new_tip = 'never',trash = False)
         p300.return_tip()
 
         #dispensing standard to stamping plate
-        for i in range(3):
+        for i in range(3): 
             p300.transfer(volume = samp_vol1[i], source = tube_rack['B1'], dest = stamping_plate[dest1[j][i]], new_tip = 'always',trash = False, mix_after=(3,200)) 
         p300.transfer(volume = samp_vol1[3], source = tube_rack['B2'], dest = stamping_plate[dest1[j][3]], new_tip = 'always',trash = False, mix_after=(3,200)) 
 
 
-# Step 2: adding 3 replicates of diluent and standard control to 0(A1-3), 15(B1-3), 25(C1-3) and 50(D1-3) mcg/ml points to the stamping plate
-
-#   dil_vol2 = [800,740,700,600] #diluent volume
-#   dil_vol2_a = 250
+# Step 3: adding 3 replicates of diluent and standard control to 0(A1-3), 15(B1-3), 25(C1-3) and 50(D1-3) mcg/ml points to the stamping plate
+#   Total volume of diluent needed is 300, 240, 200 and 100 uL,respectuively as shown in the array dil_vol2_b
+    
     dil_vol2_b = [300 , 240 , 200 , 100]
-    dest_4 = ['A1','B1','C1','D1']
+    
+    dest_4 = ['A1','B1','C1','D1'] #well locations for 0-50 ug/mL standard points are in the arrays dest_4, dest_4 and dest_6
     dest_5 = ['A2','B2','C2','D2']
     dest_6 = ['A3','B3','C3','D3']
-    dest2 =[dest_4,dest_5,dest_6]
-    samp_vol2 = [60,100,200] #these ratios will make up the dilutions from 0-50
+    
+    dest2 =[dest_4,dest_5,dest_6] #triplicating the destination since we are adding the standard in triplicate
+    
+    samp_vol2 = [60,100,200] #reference standard volume needed to make up the  0-50 ug/mL reference points
 
-    Hdest = ['H1','H2','H3'] #location of highest concentration of standard to make these smaller dilutions
+    Hdest = ['H1','H2','H3'] #taking from the reference prepared in  wells H1, H2 and H3 to prepare the 0-50ug/mL dilutions 
 
+        #replicating in 3
     for j in range (3): 
     #dispensing diluent
         p300.pick_up_tip()      
@@ -106,16 +114,16 @@ def run(protocol: protocol_api.ProtocolContext):
             p300.transfer(volume = samp_vol2[i], source = stamping_plate[Hdest[j]], dest = stamping_plate[dest2[j][i+1]], new_tip = 'always',trash = False, mix_after=(3,200))
 
 
-    #adding 280 uL of control to A4 to C4 in stamping plate
+# Step 4: adding 280 uL of SSS to wells  A4, B4 and C4 in stamping plate
     ctrl_vol = 280
     ctrl_dest = ['A4','B4','C4']
     for i in range(len(ctrl_dest)):
         p300.transfer(volume = ctrl_vol, source = tube_rack['A1'], dest = stamping_plate[ctrl_dest[i]],new_tip = 'always',trash = False)
 
-    #incubate for 30 minutes in benozanase (diluent)
+# Step 5: incubate for 30 minutes in diluent
     protocol.delay(minutes=30)
 
-#Step 3: adding 25uL of sample to 225uL reagent in final plate
+#Step 6: adding 25uL of sample to 225uL reagent in final plate
     p300_multi.pick_up_tip()
     for i in range(12):
         p300_multi.transfer(volume=225, source = pierce_reagent['A1'], dest = final_plate.columns()[i], new_tip='never',trash = False)
@@ -125,7 +133,7 @@ def run(protocol: protocol_api.ProtocolContext):
     for i in range(12):
         p300_multi.transfer(volume=25, source = stamping_plate.columns()[i], dest = final_plate.columns()[i], new_tip='always',trash = False, mix_after=(3,200))
 
-#Step 4: incubating in Pierce reagent for 5 minutes
+#Step 7: incubating in Pierce reagent for 5 minutes
     protocol.delay(minutes = 5)
 
 # Now you have successfully prepared the layout of the plate with the appropriate dilutions, setup and reagents/sample ratios. Move to a plate reader.
